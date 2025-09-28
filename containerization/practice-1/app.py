e13912946fee29d13d5693cf5fb3aca4d8f4f353
Dycouzt <dieacost@outlook.com>
2025-09-28 13:19:52 +0200

    dockerfile updated practice-1

--- containerization/practice-1/app.py
+++ containerization/practice-1/app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, from a secure container!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
